314f38f2d308c3a89413d5ae1743a1e5187628b2
Fabian Jankowski <fjankowsk@gmail.com>
2022-04-20 15:00:06 +0200

    Copy in regression tests and add git ignore file.

---
+++ tests/test_healpy.py
@@ -0,0 +1,110 @@
+#
+#   Test healpy functionality.
+#   2020 Fabian Jankowski
+#
+
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+import matplotlib.pyplot as plt
+import numpy as np
+import healpy as hp
+
+# astropy generates members dynamically, pylint therefore fails
+# disable the corresponding pylint test for now
+# pylint: disable=E1101
+
+
+print("{0:<6} {1:>10} {2:>12} {3:>12}".format("Expon", "Nside", "Res", "Npix"))
+
+print("{0:<6} {1:>10} {2:>12} {3:>12}".format("", "", "(arcsec)", "(Gpix)"))
+
+for expon in range(6, 20):
+    iside = 2**expon
+
+    print(
+        "{0:<6} {1:>10} {2:>12.1f} {3:>12.1f}".format(
+            expon,
+            iside,
+            60 * hp.nside2resol(iside, arcmin=True),
+            hp.nside2npix(iside) * 1e-9,
+        )
+    )
+
+npix = hp.nside2npix(2**13)
+print("Number of pixels: {0:.1f} Gpix".format(npix * 1e-9))
+
+print("Array size in memory")
+for expon in range(10, 15):
+    iside = 2**expon
+    npix = hp.nside2npix(iside)
+    test = np.zeros(npix, dtype=np.float32)
+
+    print("{0:<6} {1:8.1f} GB".format(expon, test.nbytes / 1024**3))
+
+    del test
+
+# make test map and populate it
+NSIDE = 2**13
+NPIX = hp.nside2npix(NSIDE)
+
+print(NSIDE, NPIX)
+
+m = np.full(NPIX, hp.UNSEEN, dtype=np.float32)
+print(m)
+print("Size in memory: {0:.1f} GB".format(m.nbytes / 1024**3))
+
+coords = SkyCoord(
+    ra=["04:37:15.8961737", "05:34:31.973", "08:35:20.61149", "16:44:49.273"],
+    dec=["-47:15:09.110714", "+22:00:52.06", "-45:10:34.8751", "-45:59:09.71"],
+    unit=(u.hour, u.deg),
+    frame="icrs",
+)
+
+for item in coords:
+    # theta [0, 2*pi], phi [0, pi]
+    ra_rad = item.ra.radian
+    dec_rad = 0.5 * np.pi - item.dec.radian
+
+    print(ra_rad, dec_rad)
+
+    vec = hp.ang2vec(dec_rad, ra_rad)
+
+    mask = hp.query_disc(
+        nside=NSIDE,
+        vec=vec,
+        radius=np.radians(0.58)
+        # radius=np.radians(0.5 * 12 / 60.0)
+    )
+
+    print(len(mask))
+
+    m[mask] = 1.0
+
+hp.cartview(
+    m,
+    badcolor="lightgray",
+    cmap="Reds",
+    coord=["C"],
+    rot=(0, 0, 0),
+    title="",
+    unit="min",
+    xsize=3200,
+)
+# hp.gnomview(m)
+# hp.cartview(m)
+# hp.orthview(m)
+hp.graticule()
+
+fig = plt.gcf()
+# ax = fig.gca()
+
+# ax.set_xlabel('RA (hr)')
+# ax.set_ylabel('Dec (deg)')
+
+fig.tight_layout()
+
+fig.savefig("skymap.pdf", bbox_inches="tight")
+
+np.save("skymap.npy", m)
+
+plt.show()
---
+++ tests/test_skymap.py
@@ -0,0 +1,250 @@
+#
+#   2020 Fabian Jankowski
+#
+
+import os
+
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+import numpy as np
+from numpy.testing import assert_equal, assert_raises
+
+from skymap import Skymap
+
+# astropy generates members dynamically, pylint therefore fails
+# disable the corresponding pylint test for now
+# pylint: disable=E1101
+
+
+def test_creation():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+
+    m = Skymap(nside=nside, quantity=quantity, unit=unit)
+    print(m)
+
+
+def test_addition_success():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+
+    m1 = Skymap(nside=nside, quantity=quantity, unit=unit)
+    print(m1)
+
+    m2 = Skymap(nside=nside, quantity=quantity, unit=unit)
+    print(m2)
+
+    mtot = m1 + m2
+    print(mtot)
+
+    for param in ["arrangement", "coordinate", "dtype", "nside", "quantity", "unit"]:
+        assert getattr(mtot, param) == getattr(m1, param)
+        assert getattr(mtot, param) == getattr(m2, param)
+
+    assert_equal(mtot.data, m1.data + m2.data)
+
+
+def test_addition_fail():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+
+    m1 = Skymap(nside=nside, quantity=quantity, unit=unit)
+    print(m1)
+
+    # nside
+    m2 = Skymap(nside=2**4, quantity=quantity, unit=unit)
+    print(m2)
+
+    with assert_raises(RuntimeError):
+        m1 + m2
+
+    # quantity
+    m3 = Skymap(nside=nside, quantity="bla", unit=unit)
+    print(m3)
+
+    with assert_raises(RuntimeError):
+        m1 + m3
+
+    # unit
+    m4 = Skymap(nside=nside, quantity=quantity, unit="bla")
+    print(m4)
+
+    with assert_raises(RuntimeError):
+        m1 + m4
+
+    # nside and quantity
+    m5 = Skymap(nside=2**4, quantity="bla", unit=unit)
+    print(m5)
+
+    with assert_raises(RuntimeError):
+        m1 + m5
+
+    # quantity and unit
+    m6 = Skymap(nside=nside, quantity="bla", unit="bla")
+    print(m6)
+
+    with assert_raises(RuntimeError):
+        m1 + m6
+
+    # nside, quantity and unit
+    m7 = Skymap(nside=2**4, quantity="bla", unit="bla")
+    print(m7)
+
+    with assert_raises(RuntimeError):
+        m1 + m7
+
+
+def test_private_access():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+
+    m = Skymap(nside=nside, quantity=quantity, unit=unit)
+
+    with assert_raises(AttributeError):
+        m.__data
+
+    with assert_raises(AttributeError):
+        m.data = None
+
+
+def test_save():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+    filename = "skymap_save_test.pkl.bz2"
+
+    m = Skymap(nside=nside, quantity=quantity, unit=unit)
+    print(m)
+
+    m.save_to_file(filename)
+
+    if not os.path.isfile(filename):
+        raise RuntimeError("File does not exist.")
+    else:
+        # remove test file
+        os.remove(filename)
+
+
+def test_load():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+    filename = "skymap_save_test.pkl.bz2"
+
+    m1 = Skymap(nside=nside, quantity=quantity, unit=unit)
+
+    m1.save_to_file(filename)
+
+    # from instance
+    m2 = m1.load_from_file(filename)
+
+    for param in ["arrangement", "coordinate", "dtype", "nside", "quantity", "unit"]:
+        assert getattr(m2, param) == getattr(m1, param)
+
+    assert_equal(m2.data, m1.data)
+
+    # using the class method
+    m3 = Skymap.load_from_file(filename)
+
+    for param in ["arrangement", "coordinate", "dtype", "nside", "quantity", "unit"]:
+        assert getattr(m3, param) == getattr(m1, param)
+
+    assert_equal(m3.data, m1.data)
+
+
+def test_save_to_fits():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+    filename = "skymap_save_test.fits"
+
+    m = Skymap(nside=nside, quantity=quantity, unit=unit)
+    print(m)
+
+    m.save_to_fits(filename)
+
+    if not os.path.isfile(filename):
+        raise RuntimeError("File does not exist.")
+    else:
+        # remove test file
+        os.remove(filename)
+
+
+def test_size():
+    nside = 2**10
+    quantity = "time"
+    unit = "min"
+
+    m = Skymap(nside=nside, quantity=quantity, unit=unit)
+    print(m.size)
+
+
+def test_add_exposure():
+    nside = 2**10
+    quantity = "time"
+    unit = "min"
+
+    m = Skymap(nside=nside, quantity=quantity, unit=unit)
+
+    coords = SkyCoord(
+        ra=["04:37:15.8961737", "05:34:31.973", "08:35:20.61149", "16:44:49.273"],
+        dec=["-47:15:09.110714", "+22:00:52.06", "-45:10:34.8751", "-45:59:09.71"],
+        unit=(u.hour, u.deg),
+        frame="icrs",
+    )
+
+    radius = np.full(len(coords), 0.58)
+    length = np.full(len(coords), 10.0)
+
+    m.add_exposure(coords, radius, length)
+    print(m)
+
+
+def test_comments():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+    comments = ["Hello, test!", "Bla", "blub"]
+
+    m = Skymap(nside=nside, quantity=quantity, unit=unit)
+
+    for item in comments:
+        m.add_comment(item)
+
+    assert len(m.comments) == len(comments)
+    assert m.comments == comments
+
+
+def test_save_comments():
+    nside = 2**8
+    quantity = "time"
+    unit = "min"
+    filename = "skymap_save_test.fits"
+    comments = ["Hello", "Bla", "Test"]
+
+    m = Skymap(nside=nside, quantity=quantity, unit=unit)
+    print(m)
+
+    for item in comments:
+        m.add_comment(item)
+
+    assert len(m.comments) == len(comments)
+    assert m.comments == comments
+
+    m.save_to_fits(filename)
+
+    if not os.path.isfile(filename):
+        raise RuntimeError("File does not exist.")
+    else:
+        # remove test file
+        os.remove(filename)
+
+
+if __name__ == "__main__":
+    import nose2
+
+    nose2.main()
