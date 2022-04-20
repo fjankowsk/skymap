0043bbf830d6f8c97b1761d8c9286a3a277fe4e0
Fabian Jankowski <fjankowsk@gmail.com>
2022-04-20 15:04:18 +0200

    Adjust for nose2 test case.

--- tests/test_healpy.py
+++ tests/test_healpy.py
@@ -14,97 +14,100 @@ import healpy as hp
 # pylint: disable=E1101
 
 
-print("{0:<6} {1:>10} {2:>12} {3:>12}".format("Expon", "Nside", "Res", "Npix"))
+def test_healpy_functionality():
+    print("{0:<6} {1:>10} {2:>12} {3:>12}".format("Expon", "Nside", "Res", "Npix"))
+    print("{0:<6} {1:>10} {2:>12} {3:>12}".format("", "", "(arcsec)", "(Gpix)"))
 
-print("{0:<6} {1:>10} {2:>12} {3:>12}".format("", "", "(arcsec)", "(Gpix)"))
+    for expon in range(6, 20):
+        iside = 2**expon
 
-for expon in range(6, 20):
-    iside = 2**expon
-
-    print(
-        "{0:<6} {1:>10} {2:>12.1f} {3:>12.1f}".format(
-            expon,
-            iside,
-            60 * hp.nside2resol(iside, arcmin=True),
-            hp.nside2npix(iside) * 1e-9,
+        print(
+            "{0:<6} {1:>10} {2:>12.1f} {3:>12.1f}".format(
+                expon,
+                iside,
+                60 * hp.nside2resol(iside, arcmin=True),
+                hp.nside2npix(iside) * 1e-9,
+            )
         )
+
+    npix = hp.nside2npix(2**13)
+    print("Number of pixels: {0:.1f} Gpix".format(npix * 1e-9))
+
+    print("Array size in memory")
+    for expon in range(10, 15):
+        iside = 2**expon
+        npix = hp.nside2npix(iside)
+        test = np.zeros(npix, dtype=np.float32)
+
+        print("{0:<6} {1:8.1f} GB".format(expon, test.nbytes / 1024**3))
+
+        del test
+
+    # make test map and populate it
+    NSIDE = 2**13
+    NPIX = hp.nside2npix(NSIDE)
+
+    print(NSIDE, NPIX)
+
+    m = np.full(NPIX, hp.UNSEEN, dtype=np.float32)
+    print(m)
+    print("Size in memory: {0:.1f} GB".format(m.nbytes / 1024**3))
+
+    coords = SkyCoord(
+        ra=["04:37:15.8961737", "05:34:31.973", "08:35:20.61149", "16:44:49.273"],
+        dec=["-47:15:09.110714", "+22:00:52.06", "-45:10:34.8751", "-45:59:09.71"],
+        unit=(u.hour, u.deg),
+        frame="icrs",
     )
 
-npix = hp.nside2npix(2**13)
-print("Number of pixels: {0:.1f} Gpix".format(npix * 1e-9))
+    for item in coords:
+        # theta [0, 2*pi], phi [0, pi]
+        ra_rad = item.ra.radian
+        dec_rad = 0.5 * np.pi - item.dec.radian
 
-print("Array size in memory")
-for expon in range(10, 15):
-    iside = 2**expon
-    npix = hp.nside2npix(iside)
-    test = np.zeros(npix, dtype=np.float32)
+        print(ra_rad, dec_rad)
 
-    print("{0:<6} {1:8.1f} GB".format(expon, test.nbytes / 1024**3))
+        vec = hp.ang2vec(dec_rad, ra_rad)
 
-    del test
+        mask = hp.query_disc(
+            nside=NSIDE,
+            vec=vec,
+            radius=np.radians(0.58)
+            # radius=np.radians(0.5 * 12 / 60.0)
+        )
 
-# make test map and populate it
-NSIDE = 2**13
-NPIX = hp.nside2npix(NSIDE)
+        print(len(mask))
 
-print(NSIDE, NPIX)
+        m[mask] = 1.0
 
-m = np.full(NPIX, hp.UNSEEN, dtype=np.float32)
-print(m)
-print("Size in memory: {0:.1f} GB".format(m.nbytes / 1024**3))
-
-coords = SkyCoord(
-    ra=["04:37:15.8961737", "05:34:31.973", "08:35:20.61149", "16:44:49.273"],
-    dec=["-47:15:09.110714", "+22:00:52.06", "-45:10:34.8751", "-45:59:09.71"],
-    unit=(u.hour, u.deg),
-    frame="icrs",
-)
-
-for item in coords:
-    # theta [0, 2*pi], phi [0, pi]
-    ra_rad = item.ra.radian
-    dec_rad = 0.5 * np.pi - item.dec.radian
-
-    print(ra_rad, dec_rad)
-
-    vec = hp.ang2vec(dec_rad, ra_rad)
-
-    mask = hp.query_disc(
-        nside=NSIDE,
-        vec=vec,
-        radius=np.radians(0.58)
-        # radius=np.radians(0.5 * 12 / 60.0)
+    hp.cartview(
+        m,
+        badcolor="lightgray",
+        cmap="Reds",
+        coord=["C"],
+        rot=(0, 0, 0),
+        title="",
+        unit="min",
+        xsize=3200,
     )
+    # hp.gnomview(m)
+    # hp.cartview(m)
+    # hp.orthview(m)
+    hp.graticule()
 
-    print(len(mask))
+    fig = plt.gcf()
+    # ax = fig.gca()
 
-    m[mask] = 1.0
+    # ax.set_xlabel('RA (hr)')
+    # ax.set_ylabel('Dec (deg)')
 
-hp.cartview(
-    m,
-    badcolor="lightgray",
-    cmap="Reds",
-    coord=["C"],
-    rot=(0, 0, 0),
-    title="",
-    unit="min",
-    xsize=3200,
-)
-# hp.gnomview(m)
-# hp.cartview(m)
-# hp.orthview(m)
-hp.graticule()
+    # fig.savefig("skymap.pdf", bbox_inches="tight")
+    # np.save("skymap.npy", m)
 
-fig = plt.gcf()
-# ax = fig.gca()
+    plt.show()
 
-# ax.set_xlabel('RA (hr)')
-# ax.set_ylabel('Dec (deg)')
 
-fig.tight_layout()
+if __name__ == "__main__":
+    import nose2
 
-fig.savefig("skymap.pdf", bbox_inches="tight")
-
-np.save("skymap.npy", m)
-
-plt.show()
+    nose2.main()
