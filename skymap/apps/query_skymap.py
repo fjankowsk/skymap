a3272948a85328122f5f6aae7e6c1af86cb5b96a
Fabian Jankowski <fjankowsk@gmail.com>
2022-05-19 15:17:03 +0100

    Add commandline options.

--- skymap/apps/query_skymap.py
+++ skymap/apps/query_skymap.py
@@ -3,24 +3,74 @@
 #   Query a Skymap
 #
 
+import argparse
+
 import astropy.units as units
 from astropy.coordinates import SkyCoord
 import matplotlib.pyplot as plt
 
 from skymap import Skymap
 
-m = Skymap.load_from_file("skymap_coherent.pkl")
 
-coords = SkyCoord(
-    ra=["08:35:20.61149"],
-    dec=["-45:10:34.8751"],
-    unit=(units.hourangle, units.deg),
-    frame="icrs",
-)
+def parse_args():
+    """
+    Parse the commandline arguments.
 
-exposure = m.query(coords, [0.1 for _ in range(len(coords))])
-print(exposure)
+    Returns
+    -------
+    args: populated namespace
+        The commandline arguments.
+    """
 
-m.show()
+    parser = argparse.ArgumentParser(
+        description="Query Skymap data.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-plt.show()
+    parser.add_argument(
+        "filename", type=str, help="The name of the input sky map file."
+    )
+
+    parser.add_argument(
+        "ra",
+        type=str,
+        help="The right ascension of the coordinate to query.",
+    )
+
+    parser.add_argument(
+        "dec",
+        type=str,
+        help="The declination of the coordinate to query.",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+#
+# MAIN
+#
+
+
+def main():
+    args = parse_args()
+
+    m = Skymap.load_from_file(args.filename)
+
+    coords = SkyCoord(
+        ra=[args.ra],
+        dec=[args.dec],
+        unit=(units.hourangle, units.deg),
+        frame="icrs",
+    )
+
+    exposure = m.query(coords, [0.1 for _ in range(len(coords))])
+    print(exposure)
+
+    m.show()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
